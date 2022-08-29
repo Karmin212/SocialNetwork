218646d20f66aafafdbc059d1fe473f76ed65995
Karmin212 <artistdramatyrg@gmail.com>
2022-08-30 00:19:29 +0300

    Style

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import style from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            <img src='https://cdn-icons-png.flaticon.com/512/263/263080.png'></img>
+            <div className={style.name}>Name</div>
+            <div className={style.title}>Your life, your rules!</div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import style from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <nav className={style.nav}>
+            <div className={`${style.item} ${style.a}`}>
+                <a>Profile</a>
+            </div>
+            <div className={`${style.item} ${style.a}`}>
+                <a>News</a>
+            </div>
+            <div className={`${style.item} ${style.a}`}>
+                <a>Subscriptions</a>
+            </div>
+            <div className={`${style.item} ${style.a}`}>
+                <a>Messages</a>
+            </div>
+            <div className={`${style.item} ${style.settings} ${style.a}`}>
+                <a>Settings</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
